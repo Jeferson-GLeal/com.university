1e95847b903e33d43cb19418bc5740586afb5769
Jeferson-Leal <jeferson.gleal@outlook.com>
2024-11-29 10:35:52 -0300

    :construction: Implementado na controller os metodos getAllUsers(), getOneUser() e deleUsers().

--- authuser/src/main/java/com/university/authuser/service/impl/UserServiceImpl.java
+++ authuser/src/main/java/com/university/authuser/service/impl/UserServiceImpl.java
@@ -1,14 +1,36 @@
 package com.university.authuser.service.impl;
 
+import com.university.authuser.models.UserModel;
 import com.university.authuser.repositories.UserRepository;
 import com.university.authuser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+
+    @Override
+    public void delete(UserModel userModel) {
+        userRepository.delete(userModel);
+    }
 
 }
